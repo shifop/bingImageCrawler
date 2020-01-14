387750c55e861de1126ae519b0bb7ea8a22c2f99
马文翔 <2877668639@qq.com>
2020-01-14 11:33:15 +0800

    添加启动脚本，修改新打印格式与方式

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ from threading import Thread
 import schedule
 from downloader import crawler
 import time
-import os
-from tools import get_root_path
+import datetime
+from tools import get_root_path, get_logger
+
+logger = get_logger('app')
 
 
 def download_imge(save_path, url):
     print('开始下载图片')
     rt = crawler(save_path, url)
-    print(rt)
+    length = len(rt)
+    time_info = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    logger.info('下载结束，当前时间：%s，总计处理图片数量：%d' % (time_info, length))
+    logger.info('信息情况如下：')
+    for x in rt:
+        logger.info('rt:%s' % (x))
 
 
 def run_job(fn1, save_path, url):
@@ -19,7 +26,7 @@ def run_job(fn1, save_path, url):
     while True:
         schedule.run_pending()
         print('一次')
-        time.sleep(60*60)
+        time.sleep(60 * 60)
 
 
 if __name__ == '__main__':
